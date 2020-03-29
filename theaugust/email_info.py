6b0330a081322aef9b8fd027ab8e80d311b5723b
mustafabhanpura <mustafatasneem53@gmail.com>
2020-03-29 20:04:34 +0530

    Changes in email.py

--- theaugust/email_info.py
+++ theaugust/email_info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS= True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER='mustafatasneem53@gmail.com'
-EMAIL_HOST_PASSWORD='mustafa350421'
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
 EMAIL_PORT=587
